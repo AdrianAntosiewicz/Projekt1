b12c16b952277e6d29b4155b49f4a0318807eed0
AdrianAntosiewicz <50593084+AdrianAntosiewicz@users.noreply.github.com>
2019-05-28 17:20:13 +0200

    k

--- projekt_gry/projekt_gry/main.cpp
+++ projekt_gry/projekt_gry/main.cpp
@@ -1,1 +1,26 @@
 #include<iostream>
+#include <iostream>
+#include<SFML/Graphics.hpp>
+#include<SFML/Window.hpp>
+#include <vector>
+using namespace sf;
+using namespace std;
+
+
+int main() {
+	RenderWindow window{ VideoMode(800,600),"Arkanoid" };
+	window.setFramerateLimit(60);
+	Event event;
+	while (true) {
+		window.clear(Color::Black);
+		window.pollEvent(event);
+		if (event.type == Event::Closed) {
+			window.close();
+			break;
+		}
+		window.display();
+	}
+
+
+	return 0;
+}
